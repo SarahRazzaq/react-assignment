a179e278eeb2ccbab24dd1a5993e3158681388ae
SarahRazzaq <sarah.razzaq@stud.sundsgarden.se>
2022-06-03 11:51:19 +0200

    form validaton changed

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
  
 function Form() {
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const [data, setData] = useState("");
-    
+
 
   
     return (
@@ -13,11 +13,11 @@ function Form() {
             <h1 className="form-h1">Fill in your information and we'll get back to you</h1>
             <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
                 <label className="form-label">Enter your name:</label><br/>
-               <input className="form-inputs" {...register("name")} placeholder="Name" /><br/>
+                <input className="form-inputs" {...register("name", { required: true, maxLength: 20 })}placeholder="Name" /><br/>
                 <label className="form-label">Enter your email:</label><br/>
-                <input className="form-inputs" {...register("email")} placeholder="Email" /><br/>
+                <input className="form-inputs" {...register("lastName", { pattern: /^[A-Za-z]+$/i })}placeholder="Email" /><br/>
                 <label className="form-label">Enter your message:</label><br/>
-                <textarea className="form-inputs" {...register("aboutYou")} placeholder="Message"  /><br/>
+                <textarea className="form-inputs" {...register("aboutYou",{ required: true, minLength: 10 })} placeholder="Message"  /><br/>
                 <input className="form-input-btn" type="submit" />
                 <div className="message-box">
                 <p>{data}</p>
